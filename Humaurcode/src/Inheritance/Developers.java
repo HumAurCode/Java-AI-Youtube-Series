01995d0a2ca24075a8f8c98946574a9dfe3aec2a
HumAurCode <labaloch835@gmail.com>
2026-07-17 12:43:35 +0500

    polymorphism

--- Humaurcode/src/Inheritance/Developers.java
+++ Humaurcode/src/Inheritance/Developers.java
@@ -3,13 +3,31 @@ package Inheritance;
 public class Developers extends Employee{
     private double projectBonus;
 
+    public Developers() {
+
+    }
+
+
     public Developers(String name, int age, double salary, double projectBonus) {
         super(name, age, salary);
         this.projectBonus = projectBonus;
     }
 
+    //polymorphism
+
     public void salary(){
         double total=this.projectBonus+ this.salary;
         System.out.println("Total salary : " + total) ;
+    }
+    public void salary(double projectBonus){
+        System.out.println(projectBonus);
+    }
+    public void salary(int projectBonus){
+        System.out.println(projectBonus);
+    }
+
+    @Override
+    public void show(){
+        System.out.println("Child class");
     }
 }
--- Humaurcode/src/Inheritance/Main.java
+++ Humaurcode/src/Inheritance/Main.java
@@ -2,8 +2,12 @@ package Inheritance;
 
 public class Main {
     static void main() {
-        Developers d = new Developers("LAtif",26,50000,20000);
-        d.salary();
+//        Developers d = new Developers("LAtif",26,50000,20000);
+//        d.salary();
+        //runtime polymorphism
+        Employee obj=new Developers();
+        obj.show();
+
         
     }
 }
